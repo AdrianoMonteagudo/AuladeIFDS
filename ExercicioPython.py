c712b8cdad68cd2a7912f66d5118bf2582be6855
AdrianoMonteagudo <adriano.monteagudo@aluno.ifsp.edu.br>
2024-09-04 10:25:02 -0300

    versão3

---
+++ ExercicioPython.py
@@ -0,0 +1,6 @@
+print ("Exercicio em Python")
+n1 = float(input("Digite a primeira nota"))
+n2 = float(input("Digite a segunda nota"))
+n3 = float(input("Digite a terceira nota"))
+M = (n1+n2+n3)/3
+print (M)
